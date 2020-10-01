ea6f6ccfb52b983e50fe00e555f613774dc202b1
preeemo <pietro.morri28@gmail.com>
2020-10-01 13:05:57 +0200

    histogram equalization fixed

--- src/com/histe.c
+++ src/com/histe.c
@@ -7,17 +7,14 @@
 #include <stdlib.h>
 
 /* =============================================================== */
-int main(int argc, char **argv)
+int main(int argc, char **argv){
 /* =============================================================== */
-{
-printf("OK\n");
   struct xvimage * image1;
 	
   if (argc != 3) {
-    fprintf(stderr, "usage: %s in1.pgm, output image\n", argv[0]);
+    fprintf(stderr, "usage: %s image.pgm, output image\n", argv[0]);
     exit(0);
   }
-  printf("OK2\n");
 
   image1 = readimage(argv[1]);  
   if (image1 == NULL)
@@ -25,11 +22,12 @@ printf("OK\n");
     fprintf(stderr, "histe: readimage failed\n");
     exit(0);
   }
-  printf("OK3\n");
+  
   if (! lhiste(image1)) {
     fprintf(stderr, "histe: function histe failed\n");
     exit(0);
   }
+  
   writeimage(image1, argv[2]);
   freeimage(image1);
 
--- src/lib/lhiste.c
+++ src/lib/lhiste.c
@@ -8,50 +8,40 @@
 #include <lhiste.h>
 
 /* ==================================== */
-uint32_t lhiste(struct xvimage * image  	/* input: image to process */  
-                                     		/* output: modified image  */       
-             )
+uint32_t lhiste(struct xvimage * image){  	/* input: image to process */  
+                                     		    /* output: modified image  */     
 /* ==================================== */
-{
-	
-	
+
   uint32_t indexpixel;
-  uint8_t *ptrimage, R[255],i ;
-  float  C[255], Rt[255];
-  uint32_t newval, val;
-  uint32_t rs, cs, N;
+  uint8_t *ptrimage,  val;
+  double  C[256], newval, R[256];
+  uint32_t rs, cs, N, i;
 
   rs = image->row_size;
   cs = image->col_size;
   N = rs * cs;
   
-   for (i = 0; i < 255; i++){
-     R[i] = 0;
-     Rt[i] = 0;
-     C[i] = 0;
-   }
-  ptrimage = UCHARDATA(image);
-  for (indexpixel = 0; indexpixel < N; indexpixel++) {
-    //val = (uint32_t)(ptrimage[indexpixel]);
-    val = (uint8_t)(ptrimage[indexpixel]);
-    R[val]++;
-    //printf("%d val \n%d R[val]\n", val, R[val]);
+  for (i = 0; i < 256; i++){      
+    R[i] = 0.;
+    C[i] = 0.;
   }
 
-  C[0] = (float)R[0]/N;
-  printf("%f\n%d\n", C[0], R[0]);
-  for (i = 1; i < 255; i++) {
-    Rt[i] = (float)R[i]/N;
-    C[i] = C[i-1] + Rt[i];
-  }
+  ptrimage = UCHARDATA(image);
   
-  //  printf("ok"); exit(0);
   for (indexpixel = 0; indexpixel < N; indexpixel++) {
-    val = (uint8_t)(ptrimage[indexpixel]);
-       newval = 255*C[val];  
-    
-    
-    //printf("%d\n%d\n", newval,(uint8_t)newval);
+    val = ptrimage[indexpixel];
+    R[val] += 1.;
+  }
+
+  C[0] = (double)(R[0])/(double)(N);
+  for (i = 1; i < 256; i++) 
+    C[i] = C[i-1] + (double)(R[i])/(double)(N);
+  
+  
+  for (indexpixel = 0; indexpixel < N; indexpixel++) {
+    val = (ptrimage[indexpixel]);
+    newval = 255*C[val];  
+      
     ptrimage[indexpixel] = (uint8_t)newval;
   }
 
